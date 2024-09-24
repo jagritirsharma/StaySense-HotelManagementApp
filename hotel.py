32c202946e8e64087b01c3cecdff9eda063dfb58
Jagriti Sharma <sharmajr@rknec.edu>
2024-09-24 14:59:36 +0530

    Added customer details layout

--- hotel.py
+++ hotel.py
@@ -1,6 +1,6 @@
 from tkinter import*
 from PIL import Image, ImageTk   #pip install pillow
-
+from customer import Cust_Win
 
 class HotelManagementSystem: 
     def __init__(self, root):
@@ -41,7 +41,7 @@ class HotelManagementSystem:
         btn_frame= Frame(main_frame, bd=1)
         btn_frame.place(x=0, y=35, width=228, height=190)
         
-        cust_btn= Button(btn_frame, text= "Customer", width=22 , font=("Montserrat",14),bg="#618264",fg="white",bd=0, cursor ="hand1")
+        cust_btn= Button(btn_frame, text= "Customer", command=self.cust_details , width=22 , font=("Montserrat",14),bg="#618264",fg="white",bd=0, cursor ="hand1")
         cust_btn.grid(row=0,column=0, pady=1)
 
         room_btn= Button(btn_frame, text= "Room", width=22 , font=("Montserrat",14),bg="#618264",fg="white",bd=0, cursor ="hand1")
@@ -80,6 +80,10 @@ class HotelManagementSystem:
 
         lblimg =Label(main_frame,image=self.photoimg5,bd=4,relief=RIDGE)
         lblimg.place(x=0,y=420,width=230,height=190)
+
+    def cust_details(self):
+      self.new_window = Toplevel(self.root)
+      self.app = Cust_Win(self.new_window)
 
 
 if __name__ == "__main__":
